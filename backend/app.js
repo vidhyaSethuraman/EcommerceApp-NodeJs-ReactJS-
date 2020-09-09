6706f79b3ab09132a31cff2ec098b5017aea3222
vidhya1204 <47454217+vidhyaSethuraman@users.noreply.github.com>
2020-09-09 16:47:56 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const appRoutes = require('./routes/appRoutes');
-const cookieParser = require('cookie-parser');
 
 var cors = require('cors')
 const app = express();
 
-app.use(cookieParser())
-app.use('/public' , express.static('public'));
 app.use(express.json());
 app.use(cors())
 
-
-app.set('view engine' ,'ejs');
 
 
 mongoose.connect('mongodb+srv://test:test@cluster0.wdsct.mongodb.net/ecommerceApp?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true ,useCreateIndex: true })
